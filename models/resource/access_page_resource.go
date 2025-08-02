102cd0a665654166e1d116b304a40190e6ff4d6c
yagisan <shoyaaoyagi@gmail.com>
2025-08-02 17:42:32 +0900

    レスポンスの時刻をISO8601形式に調整

--- models/resource/access_page_resource.go
+++ models/resource/access_page_resource.go
@@ -2,19 +2,20 @@ package resource
 
 import (
 	"shiori-server/models"
+	"shiori-server/models/util"
 	"time"
 )
 
 type AccessPageResource struct {
-	VenueAddress       string    `json:"venueAddress" example:"千葉県松戸市上本郷1-2-999"`
-	VenueAccessPageUrl string    `json:"venueAccessPageUrl" example:"https://kaijou/access"`
-	Latitude           string    `json:"latitude" example:"40.999999"`
-	Longitude          string    `json:"longitude" example:"140.999999"`
-	GatheringSpot      string    `json:"gatheringSpot" example:"松戸新田駅の近くのいなげや"`
-	GatheringDate      time.Time `json:"gatheringDate" example:"2000-01-31T00:00:00+09:00"`
-	StartingDate       time.Time `json:"startingDate" example:"2000-01-31T00:15:00+09:00"`
-	RestaurantName     string    `json:"restaurantName" example:"oisiiRestaurant"`
-	RestaurantUrl      string    `json:"restaurantUrl" example:"https://oisiiRestaurant.com"`
+	VenueAddress       string       `json:"venueAddress" example:"千葉県松戸市上本郷1-2-999"`
+	VenueAccessPageUrl string       `json:"venueAccessPageUrl" example:"https://kaijou/access"`
+	Latitude           string       `json:"latitude" example:"40.999999"`
+	Longitude          string       `json:"longitude" example:"140.999999"`
+	GatheringSpot      string       `json:"gatheringSpot" example:"松戸新田駅の近くのいなげや"`
+	GatheringDate      util.JSTTime `json:"gatheringDate" example:"2000-01-31T00:00:00+09:00"`
+	StartingDate       util.JSTTime `json:"startingDate" example:"2000-01-31T00:15:00+09:00"`
+	RestaurantName     string       `json:"restaurantName" example:"oisiiRestaurant"`
+	RestaurantUrl      string       `json:"restaurantUrl" example:"https://oisiiRestaurant.com"`
 }
 
 // コンストラクタ
@@ -35,8 +36,8 @@ func NewAccessPageResource(
 	a.Latitude = latitude
 	a.Longitude = longitude
 	a.GatheringSpot = gatheringSpot
-	a.GatheringDate = gatheringDate
-	a.StartingDate = startingDate
+	a.GatheringDate = util.JSTTime{Time: gatheringDate}
+	a.StartingDate = util.JSTTime{Time: startingDate}
 	a.RestaurantName = restrauntName
 	a.RestaurantUrl = restaurantUrl
 	return a
---
+++ models/util/jst_time.go
@@ -0,0 +1,20 @@
+package util
+
+import (
+	"fmt"
+	"time"
+)
+
+type JSTTime struct {
+	time.Time
+}
+
+// JSONマーシャル時の出力形式をカスタマイズ
+func (jt JSTTime) MarshalJSON() ([]byte, error) {
+	// 日本時間に変換
+	jst := jt.In(time.FixedZone("Asia/Tokyo", 9*60*60))
+
+	// ISO8601形式 +09:00 付きでフォーマット
+	formatted := fmt.Sprintf(`"%s"`, jst.Format("2006-01-02T15:04:05+09:00"))
+	return []byte(formatted), nil
+}
