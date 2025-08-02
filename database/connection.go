b2019796db6c4b4228816e3d239daf7ea8ef1bf6
yagisan <shoyaaoyagi@gmail.com>
2025-08-02 20:47:21 +0900

    DB接続設定の改善

--- database/connection.go
+++ database/connection.go
@@ -11,28 +11,26 @@ import (
 
 var DB *sql.DB
 
-//DBの初期化処理
-
+// DBの初期化処理
 func InitDB() {
-	//MySQLのログイン情報取得
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME"),
 	)
 
+	log.Println("DB接続開始")
 	var err error
-
-	//MySQLのログイン認証
 	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
 		log.Println("DBログインでエラーが起きました")
 		panic(err)
 	}
-
-	//MySQLの疎通確認
 	if err = DB.Ping(); err != nil {
+		log.Println("DB疎通確認でエラーが起きました")
 		panic(err)
 	}
+	log.Println("DB接続成功")
 }
